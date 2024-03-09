d8aebf51709a504be935d4232c396aeb80f94e72
Darkfeenixer <162316769+Darkfeenixer@users.noreply.github.com>
2024-03-09 12:00:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94772801923', 'KASUN', true],
+  ['94722807735', 'Feenix', true], 
   [''] 
 ] //Number of owners
 
